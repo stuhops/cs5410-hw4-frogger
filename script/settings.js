28d0f45fc0657eb67eef689b6e9f111736558511
stuhops <stuart.hopkins@aggiemail.usu.edu>
2020-04-15 19:11:28 -0600

    Distinct screen fade on key change

--- script/settings.js
+++ script/settings.js
@@ -1,9 +1,11 @@
 game.changeKey = function(value) {
+  document.getElementById('dimmer').setAttribute('style', 'display: block');
   function keyPress(event) {
     game[value] = event.key;
     document.getElementById(`control-${value}`).innerHTML = event.key;
     window.removeEventListener('keydown', keyPress);
     manageControls();
+    document.getElementById('dimmer').setAttribute('style', 'display: none');
   }
   window.addEventListener('keydown', keyPress);
 }
